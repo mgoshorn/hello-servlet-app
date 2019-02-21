15b02aab1c6641074706d4786395fa1251de8997
Mitch Goshorn <msgoshorn@gmail.com>
2019-02-21 09:54:44 -0500

    Initial commit

---
+++ src/main/java/com/revature/servlets/HelloServlet.java
@@ -0,0 +1,36 @@
+package com.revature.servlets;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.catalina.servlets.DefaultServlet;
+
+/*
+ * Interface: Servlet
+ * 
+ * GenericServlet
+ * HttpServlet
+ * DefaultServlet
+ * YourServlet
+ *
+ */
+
+/**
+ * Requests get sent to a service method.
+ * HttpServlet/DefaultServlet have this method implemented already
+ * and it automatically delegates to appropriate Http actions.
+ * Such that for:
+ * POST -> doPost
+ * GET -> doGet
+ * PUT -> doPut
+ * DELETE -> doDelete
+ * 
+ *
+ */
+public class HelloServlet extends DefaultServlet {
+		
+	@Override
+	public void service(HttpServletRequest request, HttpServletResponse response) {
+		System.out.println("Request received!");
+	}
+}
